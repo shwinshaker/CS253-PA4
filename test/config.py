2105b78be13370c3a6f3f51c6ee0f079caab7ab9
shwinshaker <740992427@qq.com>
2019-03-03 16:02:42 -0800

    clean

--- test/config.py
+++
@@ -1,6 +0,0 @@
-#!./env python
-from test import fun, build
-fun()
-build()
-print(a)
-print(b)
--- test/test.py
+++
@@ -1,9 +0,0 @@
-#!./env python
-def fun():
-    global a
-    global b
-    a = 1
-    b = 2
-
-def build():
-    print(a)
